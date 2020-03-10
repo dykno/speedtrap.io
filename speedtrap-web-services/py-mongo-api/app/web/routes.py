90618a3a50b628050f57b36944cfe99a24828a8f
M@ <matt@withani.net>
2020-03-09 22:37:09 -0700

    Added landing page templates, style sheets, and data inputs

--- speedtrap-web-services/py-mongo-api/app/web/routes.py
+++ speedtrap-web-services/py-mongo-api/app/web/routes.py
@@ -3,6 +3,7 @@ from datetime import datetime, timedelta
 from bson.objectid import ObjectId
 from flask import jsonify
 from flask import request
+from flask import render_template
 from keylookup import KeyLookup
 from mongodriver import MongoDriver
 
@@ -10,9 +11,30 @@ driver = MongoDriver()
 client = driver.db_connection('speedtest-db', 'speedtest')
 api_key_lookup = KeyLookup()
 
-@app.route('/index')
+@app.route('/')
 def index():
-    return "speedtrap.io web front end"
+    db = client.speedtest
+    collection = db.results
+    
+    query_result = collection.find()
+    results = []
+    stats = {}
+    stats['fast'] = {}
+    stats['fast']['total'] = float(0)
+    stats['fast']['count'] = float(0)
+    for doc in query_result:
+        [doc.pop(key) for key in ['client', '_id']]
+        results.append(doc)
+        if not isinstance(doc['fast'], float):
+            stats['fast']['total'] += float(doc['fast'].rstrip(' Mbps'))
+        else:
+            stats['fast']['total'] += doc['fast']
+        stats['fast']['count'] += 1
+
+    stats['fast']['avg'] = stats['fast']['total'] / stats['fast']['count']
+    print(stats)
+
+    return render_template('index.html', data = results, stats = stats)
 
 @app.route('/speedtest', methods=['GET'])
 def get_speedtest():
