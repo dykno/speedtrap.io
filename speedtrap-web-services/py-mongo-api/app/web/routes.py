a5db2545806e69e923bcbe447adc14fce553c351
M@ <matt@withani.net>
2020-03-10 21:47:33 -0700

    Added average speedtest bandwidth results (calculated from all db entries)

--- speedtrap-web-services/py-mongo-api/app/web/routes.py
+++ speedtrap-web-services/py-mongo-api/app/web/routes.py
@@ -19,21 +19,42 @@ def index():
     query_result = collection.find()
     results = []
     stats = {}
-    stats['fast'] = {}
-    stats['fast']['total'] = float(0)
-    stats['fast']['count'] = float(0)
+    
+    # Loop through all results in the database
     for doc in query_result:
+        
+        # Drop sensitive / unneeded fields
         [doc.pop(key) for key in ['client', '_id']]
+
+        # Add to a list to generate table of all results
         results.append(doc)
-        if not isinstance(doc['fast'], float):
-            stats['fast']['total'] += float(doc['fast'].rstrip(' Mbps'))
-        else:
-            stats['fast']['total'] += doc['fast']
-        stats['fast']['count'] += 1
 
-    stats['fast']['avg'] = stats['fast']['total'] / stats['fast']['count']
-    print(stats)
+        # Analyze each result to generate metrics        
+        for key in doc:
+            
+            # Disregard timestamp field
+            if key == 'timestamp':
+                continue
+            else:
+                # Make sure we have our dictionary setup correctly
+                if key not in stats:
+                    stats[key] = {}
+                    stats[key]['total'] = float(0)
+                    stats[key]['count'] = float(0)
+                
+                # Check if we're dealing with a field that is in an old format
+                # Tally up the result of each speedtest to calculate average
+                if not isinstance(doc[key], float):
+                    stats[key]['total'] += float(doc[key].rstrip(' Mbps'))
+                else:
+                    stats[key]['total'] += doc[key]
 
+            # Increment counter to calculate average
+            stats[key]['count'] += 1
+
+            # Calculate average and add it to the result's dictionary
+            stats[key]['avg'] = stats[key]['total'] / stats[key]['count']
+    
     return render_template('index.html', data = results, stats = stats)
 
 @app.route('/speedtest', methods=['GET'])
